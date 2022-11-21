4a8e22c938a1ed7b8a3113ee1487ca39efc86d74
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2022-11-21 18:35:37 +0600

    right side optional section added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,12 +4,14 @@ import { useRouteError } from 'react-router-dom';
 const NotFound = () => {
     const error = useRouteError();
     return (
-        <div id="error-page">
+        <div id="error-page" className='d-flex h-100 justify-content-center align-items-center position-absolute' style={{left:'2%',right:'2%'}}>
+            <div>
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
-                <i>{error.statusText || error.message}</i>
+               404, <i>{error.statusText || error.message}</i>
             </p>
+            </div>
     </div>
     );
 };
