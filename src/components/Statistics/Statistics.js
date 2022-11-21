289b2e989d44b2b6b0236c68e54c7c4b67204a63
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2022-11-21 14:21:54 +0600

    blog component added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>
-           <h1>This is statistics</h1> 
-        </div>
-    );
+  const quizObj = useLoaderData();
+  const { data } = quizObj;
+
+  return (
+    <div className="container p-5 text-center ">
+      <h1 className="py-4">Question data in graph</h1>
+
+      <div className="justify-content-center d-flex">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="total" stackId="a" fill="#8884d8" />
+        </BarChart>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
