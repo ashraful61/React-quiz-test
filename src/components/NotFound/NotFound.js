909f75eb5b4bdc43801278ca09b3336e7e0103da
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2022-11-21 21:44:44 +0600

    not found compoent back to home btn added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link, useRouteError } from "react-router-dom";
 
 const NotFound = () => {
-    const error = useRouteError();
-    return (
-        <div id="error-page" className='d-flex h-100 justify-content-center align-items-center position-absolute' style={{left:'2%',right:'2%'}}>
-            <div>
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-               404, <i>{error.statusText || error.message}</i>
-            </p>
-            </div>
+  const error = useRouteError();
+  return (
+    <div
+      id="error-page"
+      className="d-flex h-100 justify-content-center align-items-center position-absolute"
+      style={{ left: "2%", right: "2%" }}
+    >
+      <div>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          404, <i>{error.statusText || error.message}</i>
+        </p>
+        <Link to='/'>
+        <Button variant="primary">Back To Home</Button>{' '}
+        </Link>
+      </div>
     </div>
-    );
+  );
 };
 
 export default NotFound;
