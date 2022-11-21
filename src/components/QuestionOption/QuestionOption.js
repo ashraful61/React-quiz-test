03d140486df4810bd6a9ad824dde665e8828226f
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2022-11-21 22:40:02 +0600

    question option selection issue fix

--- src/components/QuestionOption/QuestionOption.js
+++ src/components/QuestionOption/QuestionOption.js
@@ -4,9 +4,9 @@ import { Col } from 'react-bootstrap';
 const QuestionOption = ({option, index, handleAnswer}) => {
     return (
         <Col>
-           <div className="form-check">
-                <input style={{cursor:'pointer'}} onClick={() =>handleAnswer(option)} className="form-check-input" type="radio" name="flexRadioDefault" id={index}/>
-                <label className="form-check-label" htmlFor={index}>
+           <div className="form-check" >
+                <input style={{cursor:'pointer'}} onClick={() =>handleAnswer(option)} className="form-check-input" type="radio" name="flexRadioDefault" id={option} value={option}/>
+                <label style={{cursor:'pointer'}} className="form-check-label" htmlFor={option}>
                    {option}
                 </label>
             </div>
